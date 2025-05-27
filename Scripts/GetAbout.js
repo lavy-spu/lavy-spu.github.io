932e70b51f204986ac8c2ef77f61d34d71be9d15
Vy La <113500228+lavy-spu@users.noreply.github.com>
2025-05-26 22:45:22 -0700

    update about and website

--- Scripts/GetAbout.js
+++ Scripts/GetAbout.js
@@ -17,13 +17,11 @@ function updateAboutSection(aboutData) {
     // Ensure the elements exist before setting their properties
     const thumbnailElement = document.querySelector("#pfThumbnail");
     const aboutTextElement = document.querySelector("#pfAboutText");
-    const languagesElement = document.querySelector("#pfLanguages");
     const locationElement = document.querySelector("#pfLocation");
 
-    if (thumbnailElement && aboutTextElement && languagesElement && locationElement) {
+    if (thumbnailElement && aboutTextElement && locationElement) {
         thumbnailElement.src = "Content/".concat(aboutData.Thumbnail);
         aboutTextElement.innerHTML = aboutData.AboutText; // Use innerHTML to insert HTML content
-        languagesElement.innerHTML = aboutData.Languages.join(", ");
         locationElement.innerText = aboutData.Location;
     } else {
         console.error("One or more about section elements are missing in the DOM");
